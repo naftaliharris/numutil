29d07a3cd119727a46b696110ce55c6037cab18a
Naftali Harris <naftaliharris@gmail.com>
2013-01-13 11:56:39 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Setup script
+
+from distutils.core import setup
+
+setup(name='numutil',
+      version='0.0.1',
+      description='Utilities for Parsing Textual Numbers',
+      author='Naftali Harris',
+      author_email='naftaliharris@gmail.com',
+      packages=['numutil'],
+     )
