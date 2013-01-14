55cd104945a3f490d6dc4b2197462ef6489c3746
Naftali Harris <naftaliharris@gmail.com>
2013-01-13 18:22:51 -0800

    Added three more unit tests

--- numutil/numutil.py
+++ numutil/numutil.py
@@ -1,4 +1,62 @@
 """numutil.py"""
 
-def parsenum(twine):
-    return int(twine)
+from math import log10
+
+_str2num = dict([('zero', 0), ('one', 1), ('two', 2), ('three', 3),
+    ('four', 4), ('five', 5), ('six', 6), ('seven', 7), ('eight', 8),
+    ('nine', 9), ('ten', 10), ('eleven', 11), ('twelve', 12),
+    ('thirteen', 13), ('fourteen', 14), ('fifteen', 15), ('sixteen', 16),
+    ('seventeen', 17), ('eighteen', 18), ('nineteen', 19), ('twenty', 20),
+    ('thirty', 30), ('fourty', 40), ('fifty', 50), ('sixty', 60),
+    ('seventy', 70), ('eighty', 80), ('ninety', 90), ('hundred', 100),
+    ('thousand', 10 ** 3), ('million', 10 ** 6), ('billion', 10 ** 9),
+    ('trillion', 10 ** 12), ('quadrillion', 10 ** 15),
+    ('quintillion', 10 ** 18), ('sextillion', 10 ** 21),
+    ('septillion', 10 ** 24), ('octillion', 10 ** 27),
+    ('nonillion', 10 ** 30)])
+
+_num2str = dict((y, x) for x, y in _str2num.items())
+
+def parsenum(numstr):
+    """parsenum takes a string representation of a number, and returns 
+    the number. If it doesn't find a number, it will raise a ValueError.
+
+    Example:
+    >>> parsenum('4.5 million')
+    4500000
+    """
+
+    # See if the number is of form str(num)
+    try: return int(numstr)
+    except ValueError: pass
+
+    try: return float(numstr)
+    except ValueError: pass
+
+    # See if the number just has commas
+    try: return int("".join(numstr.split(',')))
+    except ValueError: pass
+
+    try: return float("".join(numstr.split(',')))
+    except ValueError: pass
+
+    # Try to parse numstr as a word-mix
+    numstr = numstr.lower()
+    words = numstr.split(' ')
+    result = 0
+    magnitude = 0
+
+    # Failure, so raise a ValueError
+    raise ValueError("Could not parse '%s' into a number" % numstr)
+
+def sigfig_round(num, sig_figs):
+    """rounds x to a given number of significant digits"""
+    if num != 0:
+        x = round(num, -int(math.floor(log10(abs(num))) - (sig_figs - 1)))
+        return int(x) if int(x) == x else x
+    else:
+        return 0  # Can't take the log of 0
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- test/numutil_test.py
+++ test/numutil_test.py
@@ -1,41 +1,73 @@
 import unittest
 from numutil import parsenum
+from fractions import Fraction
 
 class test_parsenum(unittest.TestCase):
     """Tests the parsenum function"""
 
     def test_ints(self):
-        for twine, result in [('0', 0), ('-1', -1), ('1', 1), 
+        for numstr, result in [('0', 0), ('-1', -1), ('1', 1), 
                 ('123456789', 123456789), ('-123456789', -123456789)]:
-            self.assertEquals(parsenum(twine), result)
+            self.assertEquals(parsenum(numstr), result)
 
     def test_empty_str(self):
         self.assertRaises(ValueError, lambda: parsenum(''))
 
     def test_unit_words(self):
-        for twine, result in [('zero', 0), ('one', 1), ('two', 2), 
+        for numstr, result in [('zero', 0), ('one', 1), ('two', 2), 
                 ('three', 3), ('four', 4), ('five', 5), ('six', 6), 
                 ('seven', 7), ('eight', 8), ('nine', 9), ('ten', 10), 
                 ('eleven', 11), ('twelve', 12), ('thirteen', 13), 
                 ('fourteen', 14), ('fifteen', 15), ('sixteen', 16),
                 ('seventeen', 17), ('eighteen', 18), ('nineteen', 19),
-                ('twenty', 20), ('thirty', 30), ('fourty', 40), ('fifty', 50),
+                ('twenty', 20), ('thirty', 30), ('forty', 40), ('fifty', 50),
                 ('sixty', 60), ('seventy', 70), ('eighty', 80), 
                 ('ninety', 90)]:
-            self.assertEquals(parsenum(twine), result)
+            self.assertEquals(parsenum(numstr), result)
 
     def test_comma_ints(self):
-        for twine, result in [('1,234', 1234), ('12,345', 12345), 
+        for numstr, result in [('1,234', 1234), ('12,345', 12345), 
                 ('123,456', 123456), ('1,234,567', 1234567), 
                 ('12,345,678', 12345678), ('123,456,789', 123456789),
                 ('1,234,567,890', 1234567890)]:
-            self.assertEquals(parsenum(twine), result)
-        for twine, result in [('-1,234', -1234), ('-12,345', -12345), 
+            self.assertEquals(parsenum(numstr), result)
+        for numstr, result in [('-1,234', -1234), ('-12,345', -12345), 
                 ('-123,456', -123456), ('-1,234,567', -1234567), 
                 ('-12,345,678', -12345678), ('-123,456,789', -123456789),
                 ('-1,234,567,890', -1234567890)]:
-            self.assertEquals(parsenum(twine), result)
+            self.assertEquals(parsenum(numstr), result)
 
     def test_nonints(self):
-        for twine in ['jim', 'zerox', 'bone', 'twos', 'threek', 'fourk']:
+        for numstr in ['jim', 'zerox', 'bone', 'twos', 'threek', 'fourk']:
             self.assertRaises(ValueError, lambda: parsenum(''))
+
+    def test_newspaper_ints(self):
+        for numstr, result in [('1 thousand', 10 ** 3), ('1 million', 10 ** 6),
+                ('1 billion', 10 ** 9), ('1 trillion', 10 ** 12), 
+                ('1 quadrillion', 10 ** 15), ('1 quintillion', 10 ** 18),
+                ('1 sextillion', 10 ** 21), ('1 septillion', 10 ** 24),
+                ('1 octillion', 10 ** 27), ('1 nonillion', 10 ** 30)]:
+            self.assertEquals(parsenum(numstr), result)
+        for numstr, result in [('1.2 million', 1200000), 
+                ('12.3 million', 12300000), ('12.34 million', 12340000),
+                ('123.456789 million', 123456789)]:
+            self.assertEquals(parsenum(numstr), result)
+
+    def test_word_ints(self):
+        for numstr, result in [('twenty six', 26), 
+                ('five hundred twelve', 512), 
+                ('one thousand three hundred fifty two', 1352),
+                ('fifteen million and thirty eight', 15000038)]:
+            self.assertEquals(parsenum(numstr), result) 
+
+    def test_fractions(self):
+        for denomstr, denom in [('half', 2), ('third', 3), ('fourth', 4),
+                ('fifth', 5), ('sixth', 6), ('seventh', 7), ('eighth', 8),
+                ('ninth', 9), ('tenth', 10), ('eleventh', 11),
+                ('twelfth', 12), ('thirteenth', 13), ('fourteenth', 14),
+                ('fifteenth', 15), ('sixteenth', 16), ('seventeenth', 17),
+                ('eighteenth', 18), ('nineteenth', 19), ('twentieth', 20),
+                ('thirtieth', 30), ('fortieth', 40), ('fiftieth', 50),
+                ('sixtieth', 60), ('seventieth', 70), ('eightieth', 80),
+                ('ninetieth', 90)]:
+            self.assertEquals(parsenum('one ' + denomstr), Fraction(1, denom))
